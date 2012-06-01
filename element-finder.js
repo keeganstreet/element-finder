6f4d25a1c349db34379600b59a3dd1f6afc3f732
Keegan Street <keeganstreet@gmail.com>
2012-06-01 21:43:39 +1000

    re-ordering

--- element-finder.js
+++ element-finder.js
@@ -11,8 +11,44 @@
 		list = function (val) {
 			return val.split(',');
 		},
-		walk;
+		pluralise,
+		walk,
+		begin;
 
+	// Initialise CLI
+	program
+		.version('0.0.1')
+		.option('-p, --path <string>', 'search in this directory')
+		.option('-s, --selector <string>', 'search for this Sizzle selector')
+		.option('-x, --extension <csv list>', 'only search files with this extension (default html)', list, ['html'])
+		.option('-i, --ignore <csv list>', 'ignore files matching this pattern (default .git, .svn)', list, ['.git', '.svn'])
+		.parse(process.argv);
+
+	// Show the help if no arguments were provided
+	if (!process.argv.length) {
+		program.help();
+		return;
+	}
+
+	if (!program.path) {
+		console.log('The --path argument is required. Which directory do you want to search in?');
+		return;
+	}
+
+	if (!program.selector) {
+		console.log('The --selector argument is required. What Sizzle selector do you want to search for?');
+		return;
+	}
+
+	pluralise = function(number, singular, plural) {
+		return number === 1 ? singular : plural;
+	};
+
+	/*
+	 * Recursively loop over a directory and call the options.success callback for each file
+	 * Only return files with an extension matching the options.extension parameter
+	 * Ignore files and directories matching the options.ignore parameter
+	 */
 	walk = function (dir, options) {
 		fs.readdir(dir, function (err, files) {
 			if (err) {
@@ -56,53 +92,37 @@
 		});
 	};
 
-	program
-		.version('0.0.1')
-		.option('-p, --path <string>', 'search in this directory')
-		.option('-s, --selector <string>', 'search for this Sizzle selector')
-		.option('-x, --extension <csv list>', 'only search files with this extension (default html)', list, ['html'])
-		.option('-i, --ignore <csv list>', 'ignore files matching this pattern (default .git, .svn)', list, ['.git', '.svn'])
-		.parse(process.argv);
+	begin = function () {
+		var numberOfFiles = 0,
+			numberOfFilesWithMatches = 0;
 
-	// Show the help if no arguments were provided
-	if (!process.argv.length) {
-		program.help();
-		return;
-	}
-
-	if (!program.path) {
-		console.log('The --path argument is required. Which directory do you want to search in?');
-		return;
-	}
-
-	if (!program.selector) {
-		console.log('The --selector argument is required. What Sizzle selector do you want to search for?');
-		return;
-	}
-
-	//console.log('Loop through "' + program.path + '" and look for selector "' + program.selector + '" in file extension "' + program.extension + '"');
-
-	walk(program.path, {
-		extension: program.extension,
-		ignore: program.ignore,
-		success: function (filePath) {
-			var data = fs.readFileSync(filePath, 'utf8');
-			jsdom.env({
-				html: data,
-				src: [sizzle],
-				done: function (errors, window) {
-					var matches = window.Sizzle(program.selector),
-						matchesLen = matches.length;
-					if (matchesLen > 0) {
-						console.log('Found ' + matchesLen + (matchesLen > 1 ? ' matches' : ' match') + ' in ' + filePath);
+		walk(program.path, {
+			extension: program.extension,
+			ignore: program.ignore,
+			success: function (filePath) {
+				var data = fs.readFileSync(filePath, 'utf8');
+				numberOfFiles += 1;
+				jsdom.env({
+					html: data,
+					src: [sizzle],
+					done: function (errors, window) {
+						var matches = window.Sizzle(program.selector),
+							matchesLen = matches.length;
+						if (matchesLen > 0) {
+							numberOfFilesWithMatches += 1;
+							console.log('Found ' + pluralise(matchesLen, 'match', 'matches') + ' in ' + filePath);
+						}
+						console.log(numberOfFiles);
 					}
-				}
-			});
-		},
-		error: function (err) {
-			throw err;
-		}
-	});
+				});
+			},
+			error: function (err) {
+				throw err;
+			}
+		});
+	};
+
+	begin();
 
 }());
 
