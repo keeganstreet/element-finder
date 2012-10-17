51a8eb99b5b5d9e65526c3021b566143bd53361c
keeganstreet <keeganstreet@gmail.com>
2012-10-17 21:26:15 +1100

    Wrote more tests

--- test/test.js
+++ test/test.js
@@ -23,9 +23,34 @@ var CLIeasy = require('cli-easy'),
 
 		.arg('-d test/example-html')
 
-			.discuss('calling with -s "li"')
+			.discuss('searching for an element')
 				.arg('-s "li"')
-				.expect('should return 9 matches in 3 files', /Found 9 matches in 3 files.\s*$/)
+				.expect('should find 12 matches in 4 files', /Found 12 matches in 4 files.\s*$/)
+			.undiscuss()
+
+			.discuss('searching html files only')
+				.arg('-s "li" -x "html"')
+				.expect('should find 9 matches in 3 files', /Found 9 matches in 3 files.\s*$/)
+			.undiscuss()
+
+			.discuss('search while ignoring the subfolder')
+				.arg('-s "li" -i "subfolder"')
+				.expect('should find 6 matches in 2 files', /Found 6 matches in 2 files.\s*$/)
+			.undiscuss()
+
+			.discuss('search for a more complex selector')
+				.arg('-s ".colours > .awesome-list *:first-child"')
+				.expect('should find 1 match in 1 file', /Found 1 match in 1 file.\s*$/)
+			.undiscuss()
+
+			.discuss('search with negation pseudo-selector')
+				.arg('-s "div:not(.colours)"')
+				.expect('should find 3 matches in 3 files', /Found 3 matches in 3 files.\s*$/)
+			.undiscuss()
+
+			.discuss('testing the dom structure')
+				.arg('-s "html > body > div.colours > ul.awesome-list > li"')
+				.expect('should find 3 matches in 1 file', /Found 3 matches in 1 file.\s*$/)
 			.undiscuss()
 
 		.export(module);
